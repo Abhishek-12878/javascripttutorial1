832a2f6cdef1774673d15c35710e8de597e1cf2a
Abhishek-12878 <90446479+Abhishek-12878@users.noreply.github.com>
2023-09-22 12:01:48 +0000

    convertion

--- basics/conversationOpration.js
+++ basics/conversationOpration.js
@@ -15,7 +15,7 @@ let valueInNumber=Number(score)
 let isLoggedIn=""
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1=>true;0=>false
 //""=>false
@@ -24,5 +24,35 @@ console.log(booleanIsLoggedIn);
 let someNumber=33
 
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//************Operations*********** */
+
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+
+
+let str1="yo"
+let str2=" Abhi"
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log();
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter)
