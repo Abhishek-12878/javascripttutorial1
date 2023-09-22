8a566212aade0d6d20c2e5c618ff82e7322bd84b
Abhishek-12878 <90446479+Abhishek-12878@users.noreply.github.com>
2023-09-22 10:21:49 +0000

    datatype

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+ "use strict"; //treat all JS code newer version
+
+//  alert(3+3) we are using nodejs ,not browser
+
+let name="abhishek"
+let age=3
+let isLoggedIn=false
+let state=null
+
+//number =>2 to power 53 
+//bigint
+// string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//simbol=>unique
+
+
+//object
+
+console.log(typeof "abhishek");
---
+++ basics/variable.js
@@ -0,0 +1,19 @@
+const accountId =12344
+let accountEmail="abhsidjk@gmail.com"
+var accountPassward="09876"
+accountCity="kaithal"
+let accountState;
+
+// accountId=2 //not allowed
+
+accountEmail="lkjhgfd@gmail"
+accountPassward="12345"
+accountCity="Ambala"
+
+
+// Prefer not to use var
+// because of issue in block scope and functional scope
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassward,accountCity,accountState])
